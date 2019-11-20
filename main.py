9e6137a294d0035c02c36447e084549997ebb3b1
margheritamenegazzi <margheritamenegazzi@gmail.com>
2019-11-20 16:15:35 +0100

    Use one positional argument (symbol)

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import argparse
 from python_package import stock
-import sys
-
- # get the label of the company
-if len(sys.argv) > 1:
-    company = sys.argv[1]
-    n, price = stock.get_price(company)
-    print("Company {} has a stock value of {}$".format(price, n))
-
- # if the user does not pass a label as an input
-else:
-    print("Tell me the company you want to know about!")
-    exit()
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("symbol", 
+                        help = "The symbol associated with a company") 
+    args = parser.parse_args()
+    return args
 
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    price, name = stock.get_price(args.symbol)
+    print('Company "{}" (Symbol: {}) has a stock value of {}$.'.format(name,
+                                                               args.symbol,
+                                                               price))
