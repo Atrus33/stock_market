a46299dfb501af4c376caeeece13eaf1a74c0aa7
LeonardoAntinucci <leonardo.antinucci@student.h-farm.com>
2019-11-20 10:59:03 +0100

    Use sys.argv module for company input

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from python_package import stock
+import sys
 
-n,price=stock.get_price("AAPL")
-print("Company {} has a stock value of {}$".format(price, n))
-n,price=stock.get_price("GOOGL")
-print("Company {} has a stock value of {}$".format(price, n))
+if len(sys.argv) > 1:
+    company = sys.argv[1]
+    n,price=stock.get_price(company)
+    print("Company {} has a stock value of {}$".format(price, n))
+
+else:
+    print("Tell me the company you want to know about!")
+    exit()
+
 
 
