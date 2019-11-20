b3e218bd490cb0020908afe25590dc29d73f87e3
Alex <alexandrucatalin.duma@student.h-farm.com>
2019-11-20 18:13:27 +0100

    Add pandas file reading

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import argparse
+import pandas as pd
 from python_package import stock
 from python_package import currency_handler
 
-valid_currencies = ['dollar', 'euro', 'gbp']
+
+default_datafile = 'data/allowed_currencies.csv'
+
+def read_currency_data(datafile = default_datafile):
+    df = pd.read_csv(default_datafile, sep=";")
+    df.columns = ['currency','curr/dollar','symbol']
+    return df
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -10,7 +18,7 @@ def parse_arguments():
                         help = "The symbol associated with a company")
     parser.add_argument("-c", default = 'dollar', required = True, 
                         help = "The currency related to the stock value",
-                        choices = valid_currencies)
+                        choices = read_currency_data()['currency'].tolist())
     parser.add_argument("-v", help = "Be more verbose", action="store_true")
     args = parser.parse_args()
     return args
@@ -25,3 +33,5 @@ if __name__ == "__main__":
                                                                args.symbol,
                                                                price,
                                                                currency))
+                                                            
+
--- python_package/currency_handler.py
+++ python_package/currency_handler.py
@@ -1,4 +1,8 @@
+import os
+
 def get_adjusted_price(price, currency):
+    default_datafile = '../data/allowed_currencies.csv'
+    
     if currency == "euro":
         return price * 0.9, '€'
     else:
