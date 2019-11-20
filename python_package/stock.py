4507a48105ab3defc5f1d200b8cf8eed27a004f0
margheritamenegazzi <margheritamenegazzi@gmail.com>
2019-11-20 10:41:39 +0100

    Add documentation to stock.py

--- python_package/stock.py
+++ python_package/stock.py
@@ -5,6 +5,19 @@ import json
 fmp_URL = 'https://financialmodelingprep.com/api/v3/company/profile/%s'
 
 def get_price(company="AAPL"):
+    """Return price and name of company the label is referring to.
+    
+    Given the label of a company in the stock market, the function makes a 
+    request to the financialmodelingprep service trying to get the price and 
+    the name of the company.
+    
+    Parameters:
+    company (str): The company label
+    
+    Returns:
+    price (float): The price of the company stock
+    name (str): The name of the company
+    """
     r = requests.get(fmp_URL % company)
     data = json.loads(r.text)
     price = data['profile']['price']
