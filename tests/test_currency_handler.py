4478a617031ab278379e80d8e6d42ee3b2db84ba
margheritamenegazzi <margheritamenegazzi@gmail.com>
2019-12-10 21:56:07 +0100

    Add tests for currency_handler.py

--- tests/test_currency_handler.py
+++ tests/test_currency_handler.py
@@ -1,1 +1,33 @@
- # TEST FOR currency_handler
+import unittest
+import sys
+sys.path.append('..')
+from python_package.currency_handler import get_adjusted_price
+import pandas as pd
+
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        self.path = 'data/allowed_currencies.csv'
+        self.currency_dataframe = pd.DataFrame(columns = ['currency',
+                                                          'curr_to_dollar',
+                                                          'symbol'])
+
+        self.currency_dataframe = self.currency_dataframe.append({
+                'currency':'euro',
+                'curr_to_dollar':'0.9',
+                'symbol':'€'}, ignore_index = True)
+    
+        self.currency_dataframe.set_index('currency', inplace = True)
+        
+    def test_cannot_be_negative(self):
+        price, symbol = get_adjusted_price(-5, 'euro', self.currency_dataframe)
+        self.assertGreater(price, 0)
+
+    def test_is_string(self):
+        price, symbol = get_adjusted_price(30, 'euro', self.currency_dataframe)
+        self.assertIsInstance(symbol, str)
+
+    def tearDown(self):
+        self.path = None
+        self.currency_dataframe = None
