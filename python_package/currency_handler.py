bbfd93d674e0b1d0caf7388c99b99c51deb6ef4e
yFetz <fetz.contini@gmail.com>
2019-12-10 15:22:36 +0100

    Add docstrings in currency_handler.py

--- python_package/currency_handler.py
+++ python_package/currency_handler.py
@@ -1,6 +1,20 @@
 import os
 
 def get_adjusted_price(price, currency, currency_data):
+    """Return adjusted price and name of company the label is referring to.
+    
+    If a user wants the price value in a currency different from the dollar
+    this function computes the stock value in such currency.
+    
+    :param price: The price of the stock of the chosen company
+    :type price: float
+    :param currency: The currency name chosen by the user 
+    :type currency: string
+    :param currency_data: The dataframe containing information on currencies
+    :type currency_data: Pandas.Dataframe object 
+    :return: price and name of the company
+    :rtype: string, string
+    """
     price = price * currency_data.at[currency,'curr_to_dollar'] 
     symbol = currency_data.at[currency, 'symbol']
     return price, symbol
