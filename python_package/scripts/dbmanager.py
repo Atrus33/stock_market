aa79a6dc64cf4c3b1bc9d0933340de73d52de98c
yFetz <fetz.contini@gmail.com>
2019-12-11 17:13:11 +0100

    The script correctly inserts new user

--- python_package/scripts/dbmanager.py
+++ python_package/scripts/dbmanager.py
@@ -2,17 +2,36 @@ import sqlite3
 import random
 import hashlib
 import argparse
+import os
+db_path = os.path.abspath(os.path.join(os.getcwd(), '../../data/database.db'))
 
-def create_users_table(conn, cursor):
-    """Create the users' table if it does not exist
+conn = None
+cursor = None
+
+def open_and_create():
+    """Connect to the database
     
-    :param conn: the connection handler
-    :type conn: Connection Object
-    :param cursor: the cursor
-    :type cursor: Cursor Handler
     :return: no value
     :rtype: none
     """
+    global conn
+    global cursor
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    try:
+        cursor.execute("SELECT * FROM users")  
+    except sqlite3.OperationalError:
+        create_users_table(conn, cursor)
+   
+def create_users_table():
+    """Create the users' table if it does not exist
+    
+    :return: no value
+    :rtype: none
+    """
+       
+    global conn
+    global cursor
     # Create table
     cursor.execute('''CREATE TABLE users
                    (username VARCHAR(255) NOT NULL,
@@ -21,13 +40,9 @@ def create_users_table(conn, cursor):
                     PRIMARY KEY (username))''')
 
 
-def save_new_username(conn, cursor, username, password):
+def save_new_username(username, password):
     """Save a new user in the database
     
-    :param conn: the connection handler
-    :type conn: Connection Object
-    :param cursor: the cursor
-    :type cursor: Cursor Handler
     :param username: the username
     :type username: string
     :param password: the password
@@ -35,6 +50,9 @@ def save_new_username(conn, cursor, username, password):
     :return: no value
     :rtype: none
     """
+    
+    global conn
+    global cursor
     salt = random.randint(1, 10000)
     password = str(salt) + password
     digest = hashlib.sha256(password.encode('utf-8')).hexdigest()
@@ -43,7 +61,7 @@ def save_new_username(conn, cursor, username, password):
     conn.commit()
     
     
-def check_for_username(conn, cursor, username, password):
+def check_for_username(username, password):
     """Check the credentials of a user
         
     The user provided his credentials for authentication. If the user exists
@@ -51,15 +69,14 @@ def check_for_username(conn, cursor, username, password):
     password provided by the user is the same as the digest computed as above,
     the user is authenticated and the action is allowed.
     
-    :param conn: the connection handler
-    :type conn: Connection Object
-    :param cursor: the cursor
-    :type cursor: Cursor Handler
     :param username: the username provided by the user for the authentication
     :param password: the password provided by the user for the authentication
     :return: True if the user can be authenticated, False otherwise.
     :rtype: Boolean
     """
+    
+    global conn
+    global cursor
     rows = cursor.execute("SELECT * FROM users WHERE username=?",
                           (username,))
     conn.commit()
@@ -78,17 +95,17 @@ def parse_arguments():
             usage = "%(prog)s [options]",
             epilog = "Using SQLite3")
     
-    parser.add_argument("-add", required = False,
+    parser.add_argument("-add", required = False, default = False,
                         help = "Add username '-u' with password '-p' (Bool)",
                         action = "store_true") 
-    parser.add_argument("-rm", required = False, 
+    parser.add_argument("-rm", required = False, default = False,
                         help= "Remove username '-u' with password '-p' (Bool)",
                         action = "store_true")
     
     parser.add_argument('-u', help="add a username name (requires -p)",
-                        required = True)
+                        required = True, default = None)
     parser.add_argument('-p', help = "the username password",
-                        required = False)
+                        required = False, default = None)
     
     parser.add_argument("--version",
                         action = "version",
@@ -98,5 +115,15 @@ def parse_arguments():
 
     
 if __name__ == "__main__":
+    open_and_create()
     args = parse_arguments()
-    print(args)
+    if args.add and args.rm:
+        print("You cannot add and remove a user at the same time!")
+    elif args.add:
+        if args.u is None or args.p is None:
+            print("Please provide a proper username and password combination!")
+        else:
+            save_new_username(args.u, args.p)
+            print("Successfully inserted user {}".format(args.u))
+            
+    #print(args)
